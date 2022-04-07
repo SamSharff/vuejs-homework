8149d30548d65ac80875b2fa68b8bafc815b2f82
Sam Sharff <samanthaleec@gmail.com>
2022-04-07 11:24:39 -0400

    adds button and in put field for adding a movie; creates newMovie in data; creates addMovie function in methods

--- index.js
+++ index.js
@@ -3,9 +3,16 @@
 var App = {
   data: function () {
     return {
-      message: "World's Best Movies",
+      message: "World's Best Movies (fact, not opinion)",
       movies: ["A League of Their Own", "Apollo 13", "Toy Story", "Titanic"],
+      newMovie: "",
     };
+  },
+  methods: {
+    addMovie: function () {
+      this.movies.push(this.newMovie);
+      this.newMovie = "";
+    },
   },
 };
 
